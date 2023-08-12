9dcff275cb136e4fb8641aced49ee43aca8b04d4
Neil <Neil10241126@gmail.com>
2023-08-12 09:58:17 +0800

    commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-import './node_modules/bootstrap/js/src/modal';
-import './node_modules/bootstrap/js/src/collapse';
 import './assets/scss/all.scss';
+// import './node_modules/bootstrap/js/src/modal';
+// import './node_modules/bootstrap/js/src/collapse';
+import 'bootstrap/dist/js/bootstrap';
